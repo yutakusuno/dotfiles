79e39407bf2cdb4014bb84e1b13b740612018991
Yuta Kusuno <yutakusuno51@gmail.com>
2023-12-04 16:21:07 -0800

    chore: update brew

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -7,9 +7,8 @@ alias weathertko='curl -Acurl wttr.in/Tokyo'
 alias weatheryvr='curl -Acurl wttr.in/Vancouver'
 
 #  Repositories
-alias cddf='cd $HOME/Repositories/dotfiles'
-alias cdpw='cd $HOME/Repositories/portfolio-website'
-alias cdwwm='cd $HOME/Repositories/world-weather-map'
+alias cddf='cd $HOME/repositories/private/dotfiles'
+alias cdpw='cd $HOME/repositories/private/portfolio-website'
 
 # Ruby
 alias bclpvb='bundle config --local path vendor/bundle'
